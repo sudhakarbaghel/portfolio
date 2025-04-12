ca1c62e31f6e48beb2ffd11a6ea467a23ea3df85
your name <sudhakarsinghbaghel123321@gmail.com>
2025-04-12 23:16:11 +0530

    `Added @vercel/analytics to package-lock.json and package.json, and imported Analytics component in src/app/layout.tsx`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Leftbar from "@/components/leftbar/Leftbar";
 import SleepingCat from "@/components/sleepingCat/SleepingCat";
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics />
         <div
           style={{
             position: "fixed",
